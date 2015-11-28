49fbb7faa46621feda9e72b76a739aa09b747fab
Alexis <alexis@thecoolguy.com>
2015-11-28 01:27:07 +0100

    updated parameters in launcher

--- launcher.sh
+++ launcher.sh
@@ -1,27 +1,30 @@
 #variables
 inFile=$1
 outFile=$2
-isServer=$3
-replaceTroop=$4
-replaceSwipe=$5
-manMeta=$6
-width=$7
-height=$8
-orientation=$9
+manMeta=$3
+width=$4
+height=$5
+orientation=$6
+replaceTroop=$7
+replaceSwipe=$8
+isServer=$9
 
 if [ "$inFile" == "$outFile" ]; then
 	echo "Error! Input file and output file can´t be the same"
 	exit 1
 fi
 # human
-if [ -z "$3" -o "$3" -eq 0 ]; then
+if [ -z "$isServer" ]; then
 	echo "You are a human!"
-	awk -v manMeta=$manMeta -v isServer=$isServer -v width=$width -v height=$height -v orientation=$orientation -f scriptParser.awk $inFile
+	awk -v outFile=$outFile -v manMeta=$manMeta -v isServer=$isServer -v width=$width -v height=$height -v orientation=$orientation -f scriptParser.awk $inFile
 fi
 #server (actually there is no difference, im not sure about that yet)
-if [ -n "$3" ]; then
+if [ -n "$isServer" ]; then
 	if [ "$isServer"  -eq 1 ]; then
 		echo "You aren´t human!"
-		awk -v manMeta=$manMeta -v isServer=$isServer -v width=$width -v height=$height -v orientation=$orientation -f scriptParser.awk $inFile
-	fi
+		awk -v outFile=$outFile -v manMeta=$manMeta -v isServer=$isServer -v width=$width -v height=$height -v orientation=$orientation -f scriptParser.awk $inFile
+    elif [ "$isServer" -eq 0 ]; then
+        echo "You are a human!"
+        awk -v outFile=$outFile -v manMeta=$manMeta -v isServer=$isServer -v width=$width -v height=$height -v orientation=$orientation -f scriptParser.awk $inFile
+    fi
 fi
